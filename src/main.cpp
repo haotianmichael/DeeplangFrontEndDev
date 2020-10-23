980f87f8f0135a83a21f6997cc829a4d0c6686c2
haotianmichael <2398419426@buaa.edu.cn>
2020-10-23 10:47:50 +0800

    start debug front-end of deeplang
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
+#pragma  once
 #include <iostream>
+#include <fstream>
+#include "parsing/parsing.h"
+#include "../third-party/antlr_runtime/antlr4-runtime.h"
 
 
 using namespace std;
 
+static std::string s_infile;
+static std::string s_outfile;
+static bool s_interative_mod = false;
+static const char s_description[] = R"( Deeplang Compiler)";
 
 
-int main(void)
-{
-    
-    cout << "hao" << endl;
-    return 0;
+
+static void parseOptions(int argc, char** argv) {
+
+
 }
 
+int main(int argc, char** argv)
+{
+    parseOptions(argc, argv);    
+    
+    if(s_interative_mod) {
+        return 0; 
+    }
+    if(!s_infile.size()) {
+        return -1; 
+    }
+
+
+    std::fstream infile(s_infile);
+    if(!infile.is_open()) {
+        return -1; 
+    }
+
+
+
+    //backend===========
+
+
+    return 0;
+}
